bf47f3f5bcdcda502202cff9b6d436893389ccb2
Jyoti-Gurung <74558610+Jyoti-Gurung@users.noreply.github.com>
2022-01-14 12:00:33 -0800

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,46 @@
+/*
+Auhtor: Jyoti Gurung
+Date: 1/1122
+Project: Linked List;
+*/
+
+#include <iostream>
+#include "Node.h"
+using namespace std;
+
+
+
+void add(int newValue);
+void print(Node* next);
+
+Node* head = NULL;
+
+int main() {
+  add(5);
+  print(head);
+} 
+
+void add(int newValue) {
+  Node* current = head;
+  if (current == NULL) {
+    head = new Node();
+  }
+  else {
+    while (current->getNext() != NULL) {
+      current = current->getNext();
+    }
+    current->setNext(new Node());
+  }
+}
+
+void print(Node* next) {
+  if (next == head) {
+    cout << "List: ";
+  }
+  if (next != NULL) {
+    print(next->getNext());
+  }
+  else {
+    //do nothing
+  }
+}
