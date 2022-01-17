66761bda1908d8526742fbbfa694d4159ef50e09
Jyoti-Gurung <74558610+Jyoti-Gurung@users.noreply.github.com>
2022-01-17 00:10:48 -0800

    Update main.cpp
    New

--- main.cpp
+++ main.cpp
@@ -1,13 +1,12 @@
 /*
 Auhtor: Jyoti Gurung
 Date: 1/1122
-Project: Linked List;
+Project: Linked List; Connect Node and Student Pointers
 */
 
 #include <iostream>
 #include "Node.h"
 using namespace std;
-
 
 
 void add(int newValue);
