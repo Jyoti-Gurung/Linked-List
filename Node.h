3724de8afb6e65b674905b3a9047fa58cab7f203
Jyoti-Gurung <74558610+Jyoti-Gurung@users.noreply.github.com>
2022-01-17 19:38:24 -0800

    Create Node.h

---
+++ Node.h
@@ -0,0 +1,21 @@
+ #ifndef NODE_H
+#define NODE_H
+#include <iostream> 
+#include "Student.h"
+
+using namespace std;
+
+class Node {
+  public:
+    Node(Student* newStudent);
+    ~Node();
+    Node* getNext();
+    void setNext(Node* newNext);
+    Student* getStudent();
+
+  private:
+    Student* student;
+    Node* next;
+};
+
+#endif
