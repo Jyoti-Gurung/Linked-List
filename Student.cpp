9d7b3c25bacd1354a675d44be82053e566f038fd
Jyoti-Gurung <74558610+Jyoti-Gurung@users.noreply.github.com>
2022-01-17 22:50:51 -0800

    Update Student.cpp

--- Student.cpp
+++ Student.cpp
@@ -1,14 +1,25 @@
-#ifndef STUDENT_H
-#define STUDENT_H
+//set up the getters and setters for student and next pointers, constructor/destructor; linked with Student.h to use student stuff
+
+#ifndef NODE_H
+#define NODE_H
 #include <iostream> 
+#include "Student.h"
 
 using namespace std;
 
-class Student {
+class Node {
   public:
+    Node(Student* newStudent);
+    ~Node();
+    Node* getNext();
+    void setNext(Node* newNext);
     Student* getStudent();
+    void setStudent(Student* newStudent);
+
+  //student and next pointer variables
   private:
     Student* student;
+    Node* next;
 };
 
 #endif
