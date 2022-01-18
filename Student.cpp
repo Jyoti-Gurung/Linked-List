2859b44deefa008861ebab3e893fe063d9e8a99e
Jyoti-Gurung <74558610+Jyoti-Gurung@users.noreply.github.com>
2022-01-17 19:38:45 -0800

    Create Student.cpp

---
+++ Student.cpp
@@ -0,0 +1,14 @@
+#ifndef STUDENT_H
+#define STUDENT_H
+#include <iostream> 
+
+using namespace std;
+
+class Student {
+  public:
+    Student* getStudent();
+  private:
+    Student* student;
+};
+
+#endif
