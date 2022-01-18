2c6e8d951b5906f8a9fcb2c28e10b32236703f81
Jyoti-Gurung <74558610+Jyoti-Gurung@users.noreply.github.com>
2022-01-17 22:50:29 -0800

    Update Node.cpp

--- Node.cpp
+++ Node.cpp
@@ -1,3 +1,5 @@
+//combining Node.h and Student.h to set up getters/setters for student and next pointers, and constructor/destructor
+
 #include <iostream>
 #include "Node.h"
 #include "Student.h"
@@ -18,6 +20,10 @@ void Node::setNext(Node* newNext) {
 
 Node* Node::getNext() {
   return next;
+}
+
+void Student::setStudent(Student* newStudent) {
+  student = newStudent;
 }
 
 Student* Student::getStudent() {
